ac4422a888ac4e7c1bdb3b982381d63eeae840e1
G232323uichi <duranpachecoricardo@gmail.com>
2021-09-28 05:20:55 -0400

    se  realizaron  en  el  files del  menu de la pagina main

--- application/views/pru2.php
+++ application/views/pru2.php
@@ -13,6 +13,14 @@
      
     <h5>Bootstrap</h5>
     <div>
+  <div class="p-3 mb-2 bg-primary text-white" align="center">  DE  NATACIÓN</div>
+    <h1>Bootstrap</h1>
+    <h2>Bootstrap</h2>
+    <h3>Bootstrap</h3>
+    <h4>Bootstrap</h4>
+     
+    <h5>Bootstrap</h5>
+    <div>
         <img src="aaa1.jpg">
  
     </div>
